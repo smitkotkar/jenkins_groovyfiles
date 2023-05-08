d616eb33bb471177de1d56366d81bdb6a9cdb517
smit <smitkotkar42@gmail.com>
2023-05-08 13:45:55 +0530

    dfgds

--- studentapp-ui.groovy
+++ studentapp-ui.groovy
@@ -3,12 +3,12 @@ pipeline {
     stages {
         stage ('pull') {
             steps {
-                git branch: 'main', url: 'https://github.com/smitkotkar/jenkins.git'
+                git branch: 'main', credentialsId: 'id_rsa', url: 'git@github.com:smitkotkar/jenkins_groovyfiles.git'
             }
         }
         stage ('build') {
             steps {
-                sh '/opt/apache-maven/bin/mvn clean package'
+                sh 'mvn clean package'
             }
         }
         stage ('test') {
