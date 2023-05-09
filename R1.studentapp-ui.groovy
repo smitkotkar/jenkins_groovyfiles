423ffca10e3ea48c0357fbb8ba80b0a763f3b16c
smit <smitkotkar42@gmail.com>
2023-05-09 19:07:03 +0530

    sfd

---
+++ R1.studentapp-ui.groovy
@@ -0,0 +1,27 @@
+pipeline {
+    agent any
+    stages {
+        stage ('pull') {
+            steps {
+                git branch: 'main', url: 'https://github.com/smitkotkar/studentapp-ui.git'
+            }
+        }
+        stage ('build') {
+            steps {
+                sh '/opt/apache-maven/bin/mvn clean package'
+            }
+        }
+        stage ('test') {
+            steps {
+                withSonarQubeEnv(installationName:'sonar-server',credentialsId: 'sonar-token') { 
+                  sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=student-app'
+                }  
+            }
+        }
+        stage ('deploy') {
+            steps {
+                echo 'deploy succeed'
+            }
+        }
+    }
+}
