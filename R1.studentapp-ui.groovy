5728150c1a8a995f2a71e3640bdadeb6c90ff732
smit <smitkotkar42@gmail.com>
2023-05-11 19:01:31 +0530

    adsf

--- R1.studentapp-ui.groovy
+++ R1.studentapp-ui.groovy
@@ -18,13 +18,13 @@ pipeline {
                 }  
             }
         }
-        stage ('quality-gate') {
-            steps {
-                timeout(time: 1, unit: 'HOURS') {
-                    waitForQualityGate abortPipeline: true
-                }
-            }
-        }
+        // stage ('quality-gate') {
+        //     steps {
+        //         timeout(time: 10, unit: 'MINUTES') {
+        //             waitForQualityGate abortPipeline: true
+        //         }
+        //     }
+        // }
         stage ('deploy') {
             steps {
                 echo 'deploy succeed'
