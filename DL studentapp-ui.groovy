fabdda981a75a8a557a8a5e8b41eb108ba81fa96
smit <smitkotkar42@gmail.com>
2023-05-15 21:49:18 +0530

    kjsd

--- DL studentapp-ui.groovy
+++ DL studentapp-ui.groovy
@@ -11,11 +11,23 @@ pipeline {
                 sh '/opt/apache-maven/bin/mvn clean package'
             }
         }
-        stage ('test') {
+        // stage ('test') {
+        //     steps {
+        //         sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://172.31.31.108:9000 -Dsonar.6c2f4eb508f13bce46458baccaa055c639bcbd8e'
+        //     }
+        // }
+        stage ('test-built') {
             steps {
                 withSonarQubeEnv(installationName:'sonar-server' , credentialsId : 'sonar-token') {
                       sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.ProjectKey=student-app'
                 }  
+            }
+        }
+        stage ('quality-gate') {
+            steps {
+                timeout(time: 10 , unit: 'MINUTES') {
+                    waitForQualityGate abortPipeline: true
+                }
             }
         }
         stage ('deploy') {
