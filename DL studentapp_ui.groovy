01cb85296a7f5ae19f12c841d424558368b30b22
smit <smitkotkar42@gmail.com>
2023-05-15 22:31:57 +0530

    hg

--- DL studentapp_ui.groovy
+++ DL studentapp_ui.groovy
@@ -6,11 +6,11 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/smitkotkar/studentapp-ui.git'
             }
         }
-        stage ('build') {
-            steps {
-                sh '/opt/apache-maven/bin/mvn clean package'
-            }
-        }
+        // stage ('build') {
+        //     steps {
+        //         sh '/opt/apache-maven/bin/mvn clean package'
+        //     }
+        // }
         // stage ('test') {
         //     steps {
         //         sh '/opt/apache-maven/bin/mvn sonar:sonar -Dsonar.projectKey=studentapp -Dsonar.host.url=http://172.31.31.108:9000 -Dsonar.6c2f4eb508f13bce46458baccaa055c639bcbd8e'
